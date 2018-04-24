a0d7522fc09ebe6c006942a589c14294288c9862
AAGerasimovich <angelina.gerasimovich@yandex.com>
2018-04-24 16:13:54 +0300

    -

--- src/main/java/ru/sbt/jschool/session7/Problem1.java
+++ src/main/java/ru/sbt/jschool/session7/Problem1.java
@@ -2,6 +2,9 @@ package ru.sbt.jschool.session7;
 
 public class Problem1 {
 
+
+
+    static volatile int count = 50;
     public static void main(String[] args) {
 
 
@@ -17,14 +20,27 @@ public class Problem1 {
 
             @Override
             public void run() {
-                int n = (50 - Integer.parseInt(Thread.currentThread().getName()
-                        .substring(7, Thread.currentThread().getName().length())));
 
-                System.out.println("Hello from Thread-" +  n);
-                if (n==0){
+                int n = Integer.parseInt(Thread.currentThread().getName()
+                        .substring(7, Thread.currentThread().getName().length()));
+                if (n==51){
                     return;
                 }
+
                 threadProducer().start();
+
+                while (n!=count){
+
+                }
+                if (n==count) {
+                        System.out.println("Hello from Thread-" + n);
+                        count--;
+                        return;
+                }
+
+
+
+
             }
 
         });
